1e0357c909a97ca8bc2f5c4e804fc3dd0d221edb
deMelo1 <130903363+deMelo1@users.noreply.github.com>
2023-08-30 22:31:46 -0300

    commit4

--- screens/inicio/App.js
+++ screens/inicio/App.js
@@ -3,12 +3,20 @@ import React, {useState, useEffect} from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 import Videos from './videos';
 
+const data = [
+    { id: '1', picture: '../../assets/images/IME' },
+    { id: '2', picture: '../../assets/images/IME' },
+    { id: '3', picture: '../../assets/images/IME' },
+    { id: '4', picture: '../../assets/images/IME' },
+    { id: '5', picture: '../../assets/images/IME' },
+  ];
+
 export default function App() {
     console.disableYellowBox = true;
     return (
     <View style={{backgroundColor: 'black', width: '100%', height:'100%'}}>
-        <Text style={{color:'white', padding: 30}}>olaaaaaaa</Text>
-        <Videos text="Populares na Netflix"></Videos>
+        <Text style={{color:'white', padding: 30}}>olaaaaaaaaaaa</Text>
+        <Videos data= {data} text="Populares na Netflix"></Videos>
     </View>
     )
 }
--- screens/inicio/videos.js
+++ screens/inicio/videos.js
@@ -2,17 +2,37 @@ import { StatusBar } from 'expo-status-bar';
 import React, {useState, useEffect} from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 import { useFonts,Lato_100Thin_Italic } from '@expo-google-fonts/lato';
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity, FlatList, ImageBackground } from 'react-native';
+
+const data = [
+    { id: '1', picture: require('../../assets/images/IME.jpeg') },
+    { id: '2', picture: require('../../assets/images/IME.jpeg') },
+    { id: '3', picture: require('../../assets/images/IME.jpeg') },
+    { id: '4', picture: require('../../assets/images/IME.jpeg') },
+    { id: '5', picture: require('../../assets/images/IME.jpeg') },
+  ];
+
+const renderItem = ({ item }) => (
+        <View style={{paddingRight:12}}>
+            <ImageBackground source = {item.picture}>
+                <TouchableOpacity style={style.botao}></TouchableOpacity>
+            </ImageBackground>
+        </View>
+  );
+  
 
 export default function Videos(props) {
     return(
         <View style={style.videos}>
             <Text style={style.texto}>{props.text}</Text>
-            <View style={{flex:1, flexDirection: 'row', justifyContent: "space-between"}}>
-                <TouchableOpacity style={style.botao}></TouchableOpacity>
-                <TouchableOpacity style={style.botao}></TouchableOpacity>
-                <TouchableOpacity style={style.botao}></TouchableOpacity>
-            </View>
+            <FlatList
+                data={data}
+                renderItem={renderItem}
+                keyExtractor={item => item.id}
+                horizontal={true}
+                showsVerticalScrollIndicator={false}
+                showsHorizontalScrollIndicator={false}
+            />
         </View>
     )
 }
@@ -31,6 +51,12 @@ const style = StyleSheet.create({
     botao:{
         width: 120,
         height: '100%',
-        backgroundColor: 'white',
+        //backgroundColor: 'rgba(255,255,255,0.3)',
     }
 })
+
+/*<View style={{flex:1, flexDirection: 'row', justifyContent: "space-between"}}>
+                <TouchableOpacity style={style.botao}></TouchableOpacity>
+                <TouchableOpacity style={style.botao}></TouchableOpacity>
+                <TouchableOpacity style={style.botao}></TouchableOpacity>
+            </View>*/
