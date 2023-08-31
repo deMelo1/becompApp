49e2f5b26ae9182c81df12500e5b43b36c846554
deMelo1 <130903363+deMelo1@users.noreply.github.com>
2023-08-31 19:26:47 -0300

    commit5

--- screens/inicio/App.js
+++ screens/inicio/App.js
@@ -1,26 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useCallback} from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 import Videos from './videos';
+import * as SplashScreen from 'expo-splash-screen';
+import { useFonts,Lato_100Thin_Italic } from '@expo-google-fonts/lato';
+
+SplashScreen.preventAutoHideAsync();
 
 const data = [
-    { id: '1', picture: '../../assets/images/IME' },
-    { id: '2', picture: '../../assets/images/IME' },
-    { id: '3', picture: '../../assets/images/IME' },
-    { id: '4', picture: '../../assets/images/IME' },
-    { id: '5', picture: '../../assets/images/IME' },
+    { id: '1', picture: require('../../assets/images/boasvindasgrifo.png') },
+    { id: '2', picture: require('../../assets/images/boasvindasgrifo.png') },
+    { id: '3', picture: require('../../assets/images/boasvindasgrifo.png') },
+    { id: '4', picture: require('../../assets/images/boasvindasgrifo.png') },
+    { id: '5', picture: require('../../assets/images/boasvindasgrifo.png') },
   ];
 
 export default function App() {
     console.disableYellowBox = true;
+
+    const [fontsLoaded] = useFonts({
+        'Montserrat-Medium': require('../../assets/fonts/Montserrat-Medium.ttf'),
+      });
+    
+      const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+          await SplashScreen.hideAsync();
+        }
+      }, [fontsLoaded]);
+    
+      if (!fontsLoaded) {
+        return null;
+      }
+
     return (
-    <View style={{backgroundColor: 'black', width: '100%', height:'100%'}}>
-        <Text style={{color:'white', padding: 30}}>olaaaaaaaaaaa</Text>
-        <Videos data= {data} text="Populares na Netflix"></Videos>
+    <View style={{backgroundColor: 'rgb(25,25,25)', width: '100%', height:'100%'}}>
+        <StatusBar hidden />
+        <View style={style.header}>
+            <Text style={style.imeflix1}>IME</Text>
+            <Text style={style.imeflix2}>FLIX</Text>
+        </View>
+        <Videos data= {data} text="Populares na Imeflix"></Videos>
     </View>
     )
 }
 
 const style = StyleSheet.create({
-
+    header:{
+        width:'100%',
+        height: 80,
+        backgroundColor:'black',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent:'center',
+        paddingTop: 24,
+    },
+    imeflix1:{
+        color: 'rgb(192,42,41)',
+        fontSize: 30,
+        fontFamily: 'Montserrat-Medium',
+    },
+    imeflix2:{
+        color: 'rgb(33,85,143)',
+        fontSize: 30,
+        fontFamily: 'Montserrat-Medium',
+    }
 })
--- screens/inicio/videos.js
+++ screens/inicio/videos.js
@@ -1,20 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useCallback} from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 import { useFonts,Lato_100Thin_Italic } from '@expo-google-fonts/lato';
 import { TouchableOpacity, FlatList, ImageBackground } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
 
-const data = [
-    { id: '1', picture: require('../../assets/images/IME.jpeg') },
-    { id: '2', picture: require('../../assets/images/IME.jpeg') },
-    { id: '3', picture: require('../../assets/images/IME.jpeg') },
-    { id: '4', picture: require('../../assets/images/IME.jpeg') },
-    { id: '5', picture: require('../../assets/images/IME.jpeg') },
-  ];
+SplashScreen.preventAutoHideAsync();
 
 const renderItem = ({ item }) => (
         <View style={{paddingRight:12}}>
-            <ImageBackground source = {item.picture}>
+            <ImageBackground source = {item.picture} style={style.imagensdefundo}>
                 <TouchableOpacity style={style.botao}></TouchableOpacity>
             </ImageBackground>
         </View>
@@ -22,11 +17,25 @@ const renderItem = ({ item }) => (
   
 
 export default function Videos(props) {
+    const [fontsLoaded] = useFonts({
+        'OpenSans-SemiBold': require('../../assets/fonts/OpenSans-SemiBold.ttf'),
+      });
+    
+      const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+          await SplashScreen.hideAsync();
+        }
+      }, [fontsLoaded]);
+    
+      if (!fontsLoaded) {
+        return null;
+      }
+
     return(
         <View style={style.videos}>
             <Text style={style.texto}>{props.text}</Text>
             <FlatList
-                data={data}
+                data={props.data}
                 renderItem={renderItem}
                 keyExtractor={item => item.id}
                 horizontal={true}
@@ -40,18 +49,23 @@ export default function Videos(props) {
 const style = StyleSheet.create({
     videos:{
         width:'100%',
-        height:'29%',
+        height:'27%',
     },
     texto:{
         color:'white',
-        fontFamily: 'Lato_100Thin_Italic',
-        fontSize: 18, 
-        paddingTop: 30,
+        fontFamily: 'OpenSans-SemiBold',
+        fontSize: 24, 
+        paddingTop: 20,
     },
     botao:{
         width: 120,
         height: '100%',
         //backgroundColor: 'rgba(255,255,255,0.3)',
+    },
+    imagensdefundo:{
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center',
     }
 })
 
